f9a6c95ff3387617a01cc3f9d412a1e4287fb533
juanmanuelcrego <juanmanuel.crego@gmail.com>
2020-07-21 21:55:48 -0300

    favicon y theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `jCrego | Front End Dev`,
+        short_name: `jCrego`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#222222`,
+        theme_color: `#222222`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/jCrego Logo.png`, // This path is relative to the root of the site.
       },
     },
     {
