e9ebb5c0c9a70d695bb384da57019a3ec934ad79
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-08-08 03:19:35 -0300

    Dark theme

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
-
+import ButtonToggle from '../Theme/buttonToggle'
 import Menu from './menu-svg'
 
 const Navbar = (props) => (
     <div className='navbar'>
         <ul>
+        <li><ButtonToggle></ButtonToggle></li>
             <li><Link to="/">{props.linkHome}</Link></li>
             <li><Link to='#portfolio'>{props.link1}</Link></li>
             <li><Link to='#contact'>{props.link2}</Link></li>
