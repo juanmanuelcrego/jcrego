26bc8d206957224504c0881fa2a2595c181eecab
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-08-08 03:20:35 -0300

    Dark theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,40 @@
-import React from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header"
-import "../styles/styles.css"
+// import "../styles/styles.css"
+
+import {Global, css} from "@emotion/core"
+import {useTheme} from "emotion-theming"
+import Context from "../Theme/context"
 
 const Layout = ({ children }) => {
+
+  const {state} = useContext(Context)
+
+  const theme = useTheme()
 
   return (
     <>
       <Header />
-      <section
-      >
-        <main>{children}</main>
+      <section>
+        <Global 
+          styles={css`
+            * {
+              margin: 0;
+              padding: 0;
+            }
+
+            body {
+              background-color: ${state.isDark ? theme.dark.background : theme.light.background}
+            }
+            h1, h2, p {
+              color: ${state.isDark ? theme.dark.font : theme.light.font}
+            }
+          
+          `}
+          />          
+        {children}
       </section>
     </>
   )
