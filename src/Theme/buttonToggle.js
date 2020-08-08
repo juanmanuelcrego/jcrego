2c9aab083350fac3c271aff93870ef502c33d198
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-08-08 03:05:29 -0300

    Dark Theme

---
+++ src/Theme/buttonToggle.js
@@ -0,0 +1,26 @@
+import React, {useState, useContext} from "react"
+
+import Moon from '../images/moon.png'
+
+import {useTheme} from 'emotion-theming'
+import {css} from "@emotion/core"
+import Context from './context'
+
+const ButtonToggle = () => {
+    const {state, dispatch} = useContext(Context)
+    const theme = useTheme()
+    
+    const button = state.isDark ? <button className='themebtn light'></button> : <button className='themebtn moon'></button>
+
+return (
+        <div>
+        <button className='themebtn' onClick={() => dispatch({type: "TOGGLE_DARK_MODE"})}>{button}</button>
+        </div>
+)
+}
+
+export default ButtonToggle
+
+
+
+
