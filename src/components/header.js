c5fd0f20675f3fddda7bd94c3a4e27a715ac50df
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-08-13 19:59:52 -0300

    Dark theme mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import  Navbar from './navbar'
 import { Link } from "gatsby"
+import ButtonToggle from '../Theme/buttonToggle'
+
 
 const Header = (props) => (
   <section className="header">
@@ -10,6 +12,7 @@ const Header = (props) => (
     <Navbar linkHome="Home" link1="Portfolio" link2="Contact"/>
     <div className='linksdiv'>
                 <ul>
+                    <li><ButtonToggle></ButtonToggle></li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to='#portfolio'>Portfolio</Link></li>
                     <li><Link to='#contact'>Contact</Link></li>
