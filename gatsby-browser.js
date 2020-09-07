7f9afb35442d2e2bce378c69a31a208043566fca
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-09-07 04:47:12 -0300

    estilos

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from 'react'
 import GlobalStateProvider from './src/Theme/GlobalStateProvider'
 import { ThemeProvider } from 'emotion-theming'
 import {theme} from './src/Theme/theme'
-// import './src/styles/styles.css'
+import './src/styles/styles.css'
 import './node_modules/bootstrap/dist/css/bootstrap.css'
 
 export const wrapRootElement = ({element}) => {
