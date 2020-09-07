0b1bee636c75db8c39ba78be28a78d39b4f10dce
Juan Manuel Crego Risso <49103938+juanmanuelcrego@users.noreply.github.com>
2020-09-07 03:37:38 -0300

    grid

--- src/components/article.js
+++ src/components/article.js
@@ -80,7 +80,7 @@ const Article = () => {
   return (
     <Container fluid>
       <Row className="justify-content-md-center">
-        <Col xs={12} sm={12} md={12} lg={8} xl={8}>
+        <Col xs={2} sm={12} md={12} lg={8} xl={8}>
           <article className="about-content">
             <div className="about-text">
               <h2>About</h2>
